7cd57acd8bee2ccc8eef7705e04122d1ddee6962
luckyluk07 <L.Nojman11@wp.pl>
2022-03-11 21:35:29 +0100

    Create basic sudoku board model and basic initialization

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -6,7 +6,17 @@ namespace SudokuSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] rows = { "780400120", "780400120", "780400120", "780400120", "780400120", "780400120", "780400120", "780400120", "780400120" };
+            char[,] board = new char[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    board[i, j] = rows[i][j];
+                }
+            }
+            SudokuBoard sudoku = new SudokuBoard(board);
+            sudoku.PrintBoard();
         }
     }
 }
---
+++ SudokuSolver/SudokuBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class SudokuBoard
+    {
+        public const int MaxNumber = 9;
+        private char[,] board { get; set; }
+
+        public SudokuBoard()
+        {
+            this.board = new char[MaxNumber, MaxNumber];
+        }
+
+        public SudokuBoard(char[,] board)
+        {
+            this.board = board;
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < MaxNumber; i++)
+            {
+                for (int j = 0; j < MaxNumber; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
