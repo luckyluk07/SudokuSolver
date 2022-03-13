45210f93ba432005b5f6aab6abed72bbab1598ad
luckyluk07 <L.Nojman11@wp.pl>
2022-03-13 20:51:54 +0100

    Add square validation

--- SudokuSolver/SudokuBoard.cs
+++ SudokuSolver/SudokuBoard.cs
@@ -37,7 +37,7 @@ namespace SudokuSolver
         {
             for (int i = 0; i < MaxNumber; i++)
             {
-                if (!checkColumn(i) || !checkRow(i))
+                if (!checkColumn(i) || !checkRow(i) || !checkSquare(i))
                 {
                     return false;
                 }
@@ -50,8 +50,9 @@ namespace SudokuSolver
             HashSet<char> columnNumbers = new HashSet<char>();
             for (int y = 0; y < MaxNumber; y++)
             {
-                if (!columnNumbers.Add(board[columnIndex, y]) && board[columnIndex, y] != '0')
+                if (!columnNumbers.Add(board[y, columnIndex]) && board[y, columnIndex] != '0')
                 {
+                    Console.WriteLine($"{board[y, columnIndex]} is repeated in {columnIndex + 1} column");
                     return false;
                 }
             }
@@ -65,7 +66,40 @@ namespace SudokuSolver
             {
                 if (!rowNumbers.Add(board[rowIndex,x]) && board[rowIndex, x] != '0')
                 {
+                    Console.WriteLine($"{board[rowIndex, x]} is repeated in {rowIndex + 1} row");
                     return false;
+                }
+            }
+            return true;
+        }
+
+        private Boolean checkSquare(int squareIndex)
+        {
+            int x_diff = squareIndex % 3;
+            int y_diff;
+            if (squareIndex < 3)
+            {
+                y_diff = 0;
+            } 
+            else if (squareIndex < 6)
+            {
+                y_diff = 1;
+            } 
+            else
+            {
+                y_diff = 2;
+            }
+
+            HashSet<char> squareNumbers = new HashSet<char>();
+            for (int i = 3 * y_diff; i < (y_diff + 1) * 3; i++)
+            {
+                for (int j = 3 * x_diff; j < (x_diff + 1) * 3; j++)
+                {
+                    if(!squareNumbers.Add(board[i,j]) && board[i, j] != '0')
+                    {
+                        Console.WriteLine($"{board[i, j]} is repeated in square ({y_diff + 1} on y and {x_diff + 1} on x)");
+                        return false;
+                    }
                 }
             }
             return true;
