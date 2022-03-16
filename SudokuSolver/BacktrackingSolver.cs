c1bd63388394fb00eeb45f3435a73e820b28c76d
luckyluk07 <L.Nojman11@wp.pl>
2022-03-16 22:05:32 +0100

    Add class backtracking solver

---
+++ SudokuSolver/BacktrackingSolver.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class BacktrackingSolver
+    {
+
+        public Boolean SolveByBacktracking(char[,] board)
+        {
+            if (findEmptyPlaces(board) == null)
+            {
+                return true;
+            }
+            var emptyPlace = findEmptyPlaces(board);
+            int y = emptyPlace.Item1;
+            int x = emptyPlace.Item2;
+            for (int i = '1'; i <= '9'; i++)
+            {
+                char number = (char)i;
+                if (board[y, x] == '0' && checkPotentialPlace(board, number, x, y))
+                {
+                    board[y, x] = number;
+                    if (SolveByBacktracking(board))
+                    {
+                        return true;
+                    }
+
+                    board[y, x] = '0';
+                }
+            }
+            return false;
+        }
+
+        private Boolean checkIsSolved(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(0); j++)
+                {
+                    if (board[i, j] == '0')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private Tuple<int, int> findEmptyPlaces(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(0); j++)
+                {
+                    if (board[i, j] == '0')
+                    {
+                        return Tuple.Create(i, j);
+                    }
+                }
+            }
+            return null;
+        }
+
+        private Boolean checkPotentialPlace(char[,] board, char number, int x, int y)
+        {
+            if (board[y, x] != '0')
+            {
+                //Console.WriteLine("This place is already filled");
+                return false;
+            }
+            //check column
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[i, x] == number && i != y)
+                {
+                    return false;
+                }
+            }
+
+            //check row
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[y, i] == number && i != x)
+                {
+                    return false;
+                }
+            }
+
+            int square_x = x / 3;
+            int square_y = y / 3;
+            //check sqare
+            for (int i = square_y * 3; i < square_y * 3 + 3; i++)
+            {
+                for (int j = square_x * 3; j < square_x * 3 + 3; j++)
+                {
+                    if (board[i, j] == number && i != y && j != x)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- SudokuSolver/SudokuBoard.cs
+++ SudokuSolver/SudokuBoard.cs
@@ -11,22 +11,26 @@ namespace SudokuSolver
         public const int MaxNumber = 9;
         private char[,] board { get; set; }
         private Validator validator { get; set; }
+        private BacktrackingSolver backtrackingSolver { get; set; }
 
         public SudokuBoard()
         {
             this.board = new char[MaxNumber, MaxNumber];
             this.validator = new Validator();
+            this.backtrackingSolver = new BacktrackingSolver();
         }
 
         public SudokuBoard(char[,] board)
         {
             this.board = board;
             this.validator = new Validator();
+            this.backtrackingSolver = new BacktrackingSolver();
         }
 
         public SudokuBoard(String[] lines)
         {
             this.validator = new Validator();
+            this.backtrackingSolver = new BacktrackingSolver();
             this.board = new char[lines.Length, lines[0].Length];
             for (int i = 0; i < lines.Length; i++)
             {
@@ -44,100 +48,7 @@ namespace SudokuSolver
 
         public Boolean SolveByBacktracking()
         {
-            if (findEmptyPlaces() == null)
-            {
-                return true;
-            }
-            var emptyPlace = findEmptyPlaces();
-            int y = emptyPlace.Item1;
-            int x = emptyPlace.Item2;
-            for (int i = '1'; i <= '9'; i++)
-            {
-                char number = (char)i;
-                if (board[y, x] == '0' && checkPotentialPlace(number, x, y))
-                {
-                    board[y, x] = number;
-                    if(SolveByBacktracking())
-                    {
-                        return true;
-                    }
-
-                    board[y, x] = '0';
-                }
-            }
-            return false;
-        }
-
-        private Boolean checkIsSolved()
-        {
-            for (int i = 0; i < MaxNumber; i++)
-            {
-                for (int j = 0; j < MaxNumber; j++)
-                {
-                    if (board[i,j] == '0')
-                    {
-                        return false;
-                    }
-                }
-            }
-            return Validate();
-        }
-
-        private Tuple<int,int> findEmptyPlaces()
-        {
-            for (int i = 0; i < MaxNumber; i++)
-            {
-                for (int j = 0; j < MaxNumber; j++)
-                {
-                    if (board[i, j] == '0')
-                    {
-                        return Tuple.Create(i, j);
-                    }
-                }
-            }
-            return null;
-        }
-
-        private Boolean checkPotentialPlace(char number, int x, int y)
-        {
-            if (board[y, x] != '0')
-            {
-                //Console.WriteLine("This place is already filled");
-                return false;
-            }
-            //check column
-            for (int i = 0; i < MaxNumber; i++)
-            {
-                if (board[i, x] == number && i != y)
-                {
-                    return false;
-                }
-            }
-
-            //check row
-            for (int i = 0; i < MaxNumber; i++)
-            {
-                if (board[y,i] == number && i != x)
-                {
-                    return false;
-                }
-            }
-
-            int square_x = x / 3;
-            int square_y = y / 3;
-            //check sqare
-            for (int i = square_y * 3; i < square_y * 3 + 3; i++)
-            {
-                for (int j = square_x * 3; j < square_x * 3 + 3; j++)
-                {
-                    if (board[i,j] == number && i != y && j != x)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
+            return this.backtrackingSolver.SolveByBacktracking(board);
         }
 
         public void PrintBoard()
